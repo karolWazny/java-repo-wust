c834b3a96958d994f671c1434d3fe2ed6792b594
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-07 18:43:34 +0100

    Added directory chooser
    Added class DirectoryChooser, which is a wrapper for usage of Swing's JFileChooser.

---
+++ lab1/lib/src/lib/DirectoryChooser.java
@@ -0,0 +1,21 @@
+package lib;
+
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.Path;
+
+public class DirectoryChooser {
+    Path chooseDirectory(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        chooser.showOpenDialog(null);
+        File file = chooser.getSelectedFile();
+        return file.toPath();
+    }
+
+    public static void main(String[] args){
+        DirectoryChooser chooser = new DirectoryChooser();
+        System.out.println(chooser.chooseDirectory());
+    }
+}
