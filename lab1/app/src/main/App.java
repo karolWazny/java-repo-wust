4a56ab71c4e17bbf0b6b952c11082ea446330e00
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-07 14:03:11 +0100

    Lab 1 - initial commit

---
+++ lab1/app/src/main/App.java
@@ -0,0 +1,10 @@
+package main;
+
+import lib.Library;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello Java - Techniki Zaawansowane!");
+        Library.hello();
+    }
+}
---
+++ lab1/lib/src/lib/Library.java
@@ -0,0 +1,7 @@
+package lib;
+
+public class Library {
+    public static void hello(){
+        System.out.println("Hello, library!");
+    }
+}
