fa9986ec23901a1a535300842206074119084406
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-09 15:51:43 +0100

    Project up and working
    main.App uses full GUI, updated generated files (jars and runtime env)

--- lab1/source/applicationModule/src/main/App.java
+++ lab1/source/applicationModule/src/main/App.java
@@ -2,23 +2,8 @@ package main;
 
 import lib.*;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
 public class App {
-    public static void main(String[] args) throws IOException {
-        DirectoryChooserImpl chooser = new DirectoryChooserImpl();
-        Path directory = chooser.chooseDirectory();
-        SnapshotCreator snapper = new SnapshotCreatorImpl();
-        DirectorySnapshot snap = snapper.snapshotOf(directory);
-        SnapshotFileHandler fileHandler = new SnapshotFileHandler(directory.resolve(".snapshot"));
-        DirectorySnapshot previous = fileHandler.read();
-        if(previous != null){
-            DirectorySnapshot.Changes changes = snap.changesSince(previous);
-        } else {
-            System.out.println("This directory does not contain a '.snapshot' file.\n" +
-                    "Cannot tell what changed. It will be possible next time around.");
-        }
-        fileHandler.write(snap);
+    public static void main(String[] args) {
+        new MainWindow();
     }
 }
