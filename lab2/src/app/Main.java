f16934f803326294ec4ef44f8893a8a6628b6fa0
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-12 19:04:44 +0100

    Added directory chooser from previous project.

--- lab2/src/app/Main.java
+++ lab2/src/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
+import lib.DirectoryChooser;
+import lib.DirectoryChooserImpl;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Dupa");
+        DirectoryChooser chooser = new DirectoryChooserImpl();
+        System.out.println(chooser.chooseDirectory());
     }
 }
---
+++ lab2/src/lib/DirectoryChooser.java
@@ -0,0 +1,9 @@
+package lib;
+
+import javax.swing.*;
+import java.nio.file.Path;
+
+public interface DirectoryChooser {
+    Path chooseDirectory();
+    Path chooseDirectory(JFrame parent);
+}
