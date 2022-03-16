8528bf8400ea268d447ea9697f24613fb0829884
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-16 11:43:50 +0100

    finished record loader

--- lab2/src/lib/logic/Record.java
+++ lab2/src/lib/logic/Record.java
@@ -9,4 +9,44 @@ public class Record {
     private String lastName;
     private LocalDate birthDate;
     private String email;
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- lab2/src/lib/logic/RecordLoader.java
+++ lab2/src/lib/logic/RecordLoader.java
@@ -1,9 +1,52 @@
 package lib.logic;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.List;
 
 public class RecordLoader {
-    public Record load(Path path){
-        return null;
+    private List<String> lines;
+    private BufferedImage image;
+    private Record output;
+
+    public Record load(Path path) throws IOException {
+        lines = Files.readAllLines(path.resolve("record.txt"));
+        image = ImageIO.read(Files.newInputStream(path.resolve("image.png")));
+        return buildRecord();
+    }
+    private Record buildRecord(){
+        output = new Record();
+        processLines();
+        processImage();
+        return output;
+    }
+    private void processLines(){
+        for (String line:
+                lines){
+            int spaceIndex = line.indexOf(' ');
+            String key = line.substring(0, spaceIndex);
+            String value = line.substring(spaceIndex);
+            switch (key){
+                case "FIRST_NAME":
+                    output.setFirstName(value);
+                    break;
+                case "LAST_NAME":
+                    output.setLastName(value);
+                    break;
+                case "BIRTH_DATE":
+                    output.setBirthDate(LocalDate.parse(value));
+                    break;
+                case "EMAIL":
+                    output.setEmail(value);
+                    break;
+            }
+        }
+    }
+    private void processImage(){
+        output.setImage(image);
     }
 }
