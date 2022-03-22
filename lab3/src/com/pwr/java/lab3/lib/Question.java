e4d6dee56831d08aca3b5f1e7c5fad223fa1425b
kwazny_252716 <kwazny_252716@e-science.pl>
2022-03-22 19:17:15 +0100

    created classes for two first questions

--- lab3/src/com/pwr/java/lab3/lib/Question.java
+++ lab3/src/com/pwr/java/lab3/lib/Question.java
@@ -1,12 +1,24 @@
 package com.pwr.java.lab3.lib;
 
+import com.pwr.java.lab3.lib.DataSource;
+
+import java.util.Locale;
 import java.util.Objects;
 
 public abstract class Question {
     protected DataSource dataSource;
+    private Locale locale;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = Objects.requireNonNull(dataSource);
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 
     public abstract void setParameters(String[] parameters);
---
+++ lab3/src/com/pwr/java/lab3/lib/questions/AdminDivisionsQuestion.java
@@ -0,0 +1,30 @@
+package com.pwr.java.lab3.lib.questions;
+
+import com.pwr.java.lab3.lib.Question;
+
+public class AdminDivisionsQuestion extends Question {
+    @Override
+    public void setParameters(String[] parameters) {
+
+    }
+
+    @Override
+    public String getQuestion() {
+        return null;
+    }
+
+    @Override
+    public void answer(String answer) {
+
+    }
+
+    @Override
+    public String getAnswerComment() {
+        return null;
+    }
+
+    @Override
+    public boolean wasAnswerCorrect() {
+        return false;
+    }
+}
