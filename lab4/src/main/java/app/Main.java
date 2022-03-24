c3661b9e1bbe87666aad1ba0ec30897aa2eac7a4
kwazny_252716 <63009342+karolWazny@users.noreply.github.com>
2022-03-24 12:03:51 +0100

    added MainWindow, now it's used in Main::main

--- lab4/src/main/java/app/Main.java
+++ lab4/src/main/java/app/Main.java
@@ -1,7 +1,9 @@
 package app;
 
+import gui.MainWindow;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello there!");
+        new MainWindow();
     }
 }
