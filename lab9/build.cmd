44cb420cce5ab6a3f1a24b3c9beeb76a9cf54df3
kwazny_252716 <kwazny_252716@e-science.pl>
2022-04-29 13:20:47 +0200

    build and run configurations

--- lab9/build.cmd
+++ lab9/build.cmd
@@ -1,8 +1,17 @@
+@echo off
+set app_version=1.0-SNAPSHOT
+set lib_version=1.0-SNAPSHOT
+
 call mvn clean -f source
 call mvn package -f source
 rm -rf release
 mkdir release
+
 copy source\app\target\app*.jar release
 ren release\app*.jar app.jar
+set app_lib_dir=source\app\target\app-%app_version%.lib
+mkdir release\app-%app_version%.lib
+xcopy %app_lib_dir% release
+
 copy source\library\target\library*.jar release
 ren release\library*.jar library.jar
--- lab9/source/app/src/main/java/main/App.java
+++ lab9/source/app/src/main/java/main/App.java
@@ -1,7 +1,9 @@
 package main;
 
+import gui.MainWindow;
+
 public class App {
     public static void main(String[] args){
-        System.out.println("Kiszka.");
+        new MainWindow();
     }
 }
