2181c43e15e21c84b5f72008f95a7ae28d515bf9
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-01 23:55:13 +0200

    added example xsd file

--- lab8/src/main/java/pl/edu/pwr/java/lab8/Lab8Application.java
+++ lab8/src/main/java/pl/edu/pwr/java/lab8/Lab8Application.java
@@ -1,14 +1,15 @@
 package pl.edu.pwr.java.lab8;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import pl.edu.pwr.java.lab8.gui.MainWindow;
 import pl.edu.pwr.java.lab8.model.entity.Installment;
 import pl.edu.pwr.java.lab8.model.entity.Person;
+import pl.edu.pwr.java.lab8.service.InstallmentService;
+import pl.edu.pwr.java.lab8.service.PersonService;
 import pl.edu.pwr.java.lab8.service.ReminderService;
 
 import java.util.HashMap;
@@ -18,8 +19,11 @@ import java.util.Map;
 @Slf4j
 @SpringBootApplication
 @EnableScheduling
-public class Lab8Application extends MainWindow {
-    private ReminderService reminderService;
+@RequiredArgsConstructor
+public class Lab8Application {
+    private final ReminderService reminderService;
+    private final PersonService personService;
+    private final InstallmentService installmentService;
 
     public static void main(String[] args) {
         SpringApplication.run(Lab8Application.class, args);
@@ -58,10 +62,5 @@ public class Lab8Application extends MainWindow {
                 allPending.put(person, pendingInstallments);
         }
         reminderService.remind(allPending);
-    }
-
-    @Autowired
-    public void setReminderService(ReminderService reminderService) {
-        this.reminderService = reminderService;
     }
 }
