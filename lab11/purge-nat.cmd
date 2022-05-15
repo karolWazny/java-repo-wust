627c209a334abd3ceff7b6c45a1d20b5916d4045
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-15 18:58:54 +0200

    finishing touches

---
+++ lab11/purge-nat.cmd
@@ -0,0 +1,2 @@
+rm -rf nat-side/CMakeFiles nat-side/Release nat-side/x64 nat-side/native.dir
+rm nat-side/*.vcxproj nat-side/cmake_install.cmake nat-side/CMakeCache.txt nat-side/*.sln nat-side/*.filters
