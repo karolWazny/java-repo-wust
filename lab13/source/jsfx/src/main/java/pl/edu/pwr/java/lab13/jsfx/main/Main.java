501ff0301bba4917ad3b88ca77acafd1c441fce0
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-28 17:52:18 +0200

    buid and run scripts

---
+++ lab13/source/jsfx/src/main/java/pl/edu/pwr/java/lab13/jsfx/main/Main.java
@@ -0,0 +1,9 @@
+package pl.edu.pwr.java.lab13.jsfx.main;
+
+import pl.edu.pwr.java.lab13.jsfx.HelloApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
