0a2a2a2033272823075f2632049b4cb829fff3c5
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-28 15:39:38 +0200

    updated build script

--- lab12/build.cmd
+++ lab12/build.cmd
@@ -4,3 +4,4 @@ call mvn -f source/cell-inator verify
 rm -rf release
 mkdir release
 cp source/cell-inator/target/cell-inator-1.0-SNAPSHOT.jar release
+cp source/cell-inator/target/cell-inator-1.0-SNAPSHOT.lib/* release
