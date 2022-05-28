17e65a1a8424a5dc81136fa0ae9d18e34001cbe8
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-28 03:31:32 +0200

    game of life ready

--- lab12/source/cell-inator/src/main/java/engine/Engine.java
+++ lab12/source/cell-inator/src/main/java/engine/Engine.java
@@ -3,6 +3,10 @@ package engine;
 import javax.script.Invocable;
 import javax.script.ScriptException;
 import java.awt.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Engine {
@@ -24,33 +28,48 @@ public class Engine {
     }
 
     public Color colorForState(int state){
-        switch (state){
-            case 0:
-                return Color.CYAN;
-            case 1:
-                return Color.ORANGE;
+        try {
+            String color = (String) invocable.invokeFunction("colorForState", state);
+            return Color.decode(color);
+        } catch (ScriptException | NoSuchMethodException e) {
+            e.printStackTrace();
         }
         return Color.WHITE;
     }
 
     public int nextState(int state){
-        return (state + 1) % 2;
+        try {
+            return ((Double)invocable.invokeFunction("cycleThroughStates", state)).intValue();
+        } catch (ScriptException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     private int stateFromNeighbourhood(int[][] neighbourhood){
-        return (neighbourhood[1][1] + 1) % 2;
+        try {
+            int[] cells = new int[9];
+            IntStream.range(0, 3)
+                    .forEach(i -> IntStream.range(0, 3)
+                            .forEach(j -> {
+                                cells[i * 3 + j] = neighbourhood[i][j];
+                            }));
+            //System.out.println(Arrays.stream(cells).boxed().collect(Collectors.toList()));
+            return (int) invocable.invokeFunction("evaluateState", (Object) cells);
+        } catch (ScriptException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public void step(){
         int[][] nextState = IntStream.range(0, map.getHeight())
-                .mapToObj(y->{
-                    return IntStream.range(0, map.getWidth())
-                            .map(x->{
-                                int[][] neighbourhood = map.getNeighbourhood(new Position(x, y));
-                                return stateFromNeighbourhood(neighbourhood);
-                            })
-                            .toArray();
-                }).toArray(int[][]::new);
+                .mapToObj(y-> IntStream.range(0, map.getWidth())
+                        .map(x->{
+                            int[][] neighbourhood = map.getNeighbourhood(new Position(x, y));
+                            return stateFromNeighbourhood(neighbourhood);
+                        })
+                        .toArray()).toArray(int[][]::new);
         map.setCells(nextState);
     }
 
