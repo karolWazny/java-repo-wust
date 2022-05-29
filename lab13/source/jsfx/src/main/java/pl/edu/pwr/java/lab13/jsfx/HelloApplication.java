59ec87e7ca613797f412bbaa20325cac276c18fc
kwazny_252716 <kwazny_252716@e-science.pl>
2022-05-29 20:44:49 +0200

    choosing data file

--- lab13/source/jsfx/src/main/java/pl/edu/pwr/java/lab13/jsfx/HelloApplication.java
+++ lab13/source/jsfx/src/main/java/pl/edu/pwr/java/lab13/jsfx/HelloApplication.java
@@ -3,8 +3,8 @@ package pl.edu.pwr.java.lab13.jsfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -15,9 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
-        ChoiceBox<String> box = new ChoiceBox<>();
-        box.getItems().add("");
     }
 
     public static void main(String[] args) {
